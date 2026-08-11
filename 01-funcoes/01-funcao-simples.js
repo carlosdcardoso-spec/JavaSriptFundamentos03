12e9ea8180ce624a25cfb10aef877bb4589fb83f
Carlos <carlos.d.cardoso@edu.senai.br>
2026-08-11 16:49:48 -0300

    SalvandoMudancas

---
+++ 01-funcoes/01-funcao-simples.js
@@ -0,0 +1,23 @@
+function saudacao(){
+    console.log("Olá, Turma");
+}
+
+saudacao();
+
+function bomDia(){
+    console.log("Bom dia")
+}
+
+bomDia();
+
+function apresentar(nome){
+    console.log("Olá, "+nome);
+
+}
+apresentar("Thiago")
+
+
+
+
+
+

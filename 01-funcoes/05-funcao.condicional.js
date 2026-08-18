fe8de6932df321e3ad1055e174b91e154ffaee23
Carlos <carlos.d.cardoso@edu.senai.br>
2026-08-18 11:32:08 -0300

    Salvandoalteracao

---
+++ 01-funcoes/05-funcao.condicional.js
@@ -0,0 +1,11 @@
+function verificaNota(media){
+    // if(media> 7){
+    //     return "Aprovado"
+
+    // }
+    // return "Reprovado"
+    return media >= 7 ? "Aprovado" : "Reprovado";
+
+}
+
+console.log(verificaNota(8));
